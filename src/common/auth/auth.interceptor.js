fbfb619ab4b6860213428b436999b0a72fb96b8c
Jeffrey Gao <Jeffrey.Gao@crypteron.com>
2015-07-14 15:59:50 -0700

    Fixed login input placeholder, fixed bug with notification not being removed upon login.

--- src/common/auth/auth.interceptor.js
+++ src/common/auth/auth.interceptor.js
@@ -78,10 +78,7 @@
                 //localizedNotifications.add('login.required', 'warning', { pageTitle: $state.current.data.pageTitle }, null);
                 $state.go(auth.loginState, null, { reload: true });
             }
-        } else if (rejection.status === 400) {
-            // Bad request
-            localizedNotifications.addForNext('login.failed', 'warning', null, null);
-        }
+        } 
         return $q.reject(rejection);
     };
 
