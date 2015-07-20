90e965b92caa339c00ed44f44ead986b7cea343c
Landy Chan <landy.chan@crypteron.com>
2015-07-20 10:22:46 -0700

    Created medications HTML and JS files

---
+++ src/app/medications/medications.js
@@ -0,0 +1,32 @@
+﻿angular.module('cloudmedic.medications', [
+    'ui.router',
+    'chart.js',
+    'crypteron.resources'
+])
+.config(['$stateProvider', function config($stateProvider) {
+    $stateProvider.state('medications', {
+        url: '/medications',
+        views: {
+            "main": {
+                controller: 'MedicationsCtrl',
+                templateUrl: 'medications/medication.tpl.html'
+            }
+        },
+        resolve: {
+            security: ['$q', 'auth', function($q, auth) {
+                if(!auth.status.token || !auth.status.token.userRole.contains('SysAdmin') || !auth.status.token.userRole.contains('Physician')) {
+                    return $q.reject("Not Authorized");
+                }
+            }],
+            medications: ['Medications', function (Medications) {
+                return Medications.query().$promise;
+            }]
+        },
+        data: { pageTitle: 'Medications' }
+    });
+}])
+.controller('MedicationsCtrl', function ($scope, $state, medications, Medications, localizedNotifications, $modal) {
+
+
+
+});
