319677186449e4a9fb70d4ec08b1f7821fd9af85
Landy Chan <landy.chan@crypteron.com>
2015-08-06 11:18:28 -0700

    Updated supporter page to show careteams and patient's medication history

--- src/common/resources/users.resource.js
+++ src/common/resources/users.resource.js
@@ -13,10 +13,12 @@
             'supporters': { method: 'GET', isArray: true, params: { path: 'Supporters' } },
             'meds': { method: 'GET', isArray: true, params: { path: 'Prescriptions' } },
             'prTeams': { method: 'GET', isArray: true, params: { path: 'Provider' } },
+            'sTeams': {method: 'GET', isArray: true, params: {path: 'CareTeams'} },
             //'supporter': { method: 'POST', isArray: false, params: { path: 'Supporter' } },
             'patients': { method: 'GET', isArray: true, params: { path: 'Patients' } },
             'remove': { method: 'DELETE', isArray: false },
             'create': { method: 'POST', isArray: false, params: { path: 'Add' } }
+
         });
         return service;
     };
